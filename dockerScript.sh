c8171842a5e3cd366446a053e126e79ee0b9a175
naica922 <naima.cavegn@edu.tbz.ch>
2025-03-06 10:55:33 +0100

    Create bash script to pull, run and run docker

--- dockerScript.sh
+++ dockerScript.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo "This is a docker script!"
+docker login -u naica922 -p $DOCKER_PASSWORD
+docker build -t naica922/app:latest .
+docker push naica922/app:latest
+docker pull naica922/app:latest
+docker run -p 80:8080 naica922/app:latest
